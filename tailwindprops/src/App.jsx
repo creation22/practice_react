2aa9046048673bbd4adbaa88b5c4fbf96d347bfd
creation <creation2224@gmail.com>
2025-04-01 21:59:33 +0530

    card project

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Card from "./Components/Card"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  <h1 className='bg-green-300 text-white mb-4 text-3xl'>Tailwind Testing</h1>
+  <Card username = "virat kohli"/>
+  <Card username = "ms dhoni"/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    </>
+  )
+}
+
+export default App
