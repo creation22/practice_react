335b19446e28c1d244f876c2557ccf2f941da6f0
creation <creation2224@gmail.com>
2025-04-05 19:13:05 +0530

    reduxtoolkit

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+
+
+export default App
---
+++ reduxToolkit/src/App/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+import todoReducers from '../Features/todo/todoSlice'
+
+export const store = configureStore({
+    reducer :     todoReducers
+
+})
+
+
